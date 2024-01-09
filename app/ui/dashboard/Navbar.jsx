5c33e22f98b016ad6cad9b1c6c5e4997912f5c3e
div-vik <shivamambast5@gmail.com>
2024-01-09 18:10:10 +0530

    Complete design

---
+++ app/ui/dashboard/Navbar.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Search from "./Search";
+
+const Navbar = () => {
+  return (
+    <div className=" flex justify-between items-center px-10 py-3 shadow-sm">
+      <div className="flex gap-5 items-center">
+        <h1 className="text-xl font-[500] text-[#1A181E]">Payouts</h1>
+        <div className="flex gap-2 text-[#4D4D4D] cursor-pointer">
+          <Image
+            className="object-contain"
+            src="/help.png"
+            alt="help"
+            width={15}
+            height={15}
+          />
+          <p className="text-sm font-[400]">How it works</p>
+        </div>
+      </div>
+
+      <div className="bg-[#F2F2F2] rounded-[6px] w-[30%]">
+        <Search placeholder="Search features, tutorials, etc." />
+      </div>
+
+      <div className="flex gap-5 items-center">
+        <div className="bg-[#E6E6E6] object-contain rounded-full px-3 py-3 cursor-pointer">
+          <Image src="/chat.png" alt="chat" width={20} height={20} />
+        </div>
+        <div className="bg-[#E6E6E6] object-contain rounded-full px-4 py-5 cursor-pointer">
+          <Image src="/solidDown.png" alt="chat" width={15} height={15} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/ui/dashboard/Search.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+
+const Search = ({ placeholder }) => {
+  return (
+    <div className="flex w-full bg-transparent items-center px-5 gap-2">
+      <div>
+        <Image
+          className="object-contain py-2 bg-transparent"
+          src="/search.png"
+          alt="search"
+          width={20}
+          height={20}
+        />
+      </div>
+      <div className="w-full">
+        <input
+          className="bg-transparent py-2 w-full focus:outline-none"
+          type="text"
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
