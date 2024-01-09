f1504dbbf29ce42a0f034b1e941dc16db6fd72cf
div-vik <shivamambast5@gmail.com>
2024-01-10 01:28:44 +0530

    Complete card design

---
+++ app/ui/dashboard/Card.jsx
@@ -0,0 +1,48 @@
+import { MdArrowForwardIos } from "react-icons/md";
+import Image from "next/image";
+import { useState } from "react";
+
+const Cards = ({ title, amount, orders }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(!isHovered)}
+      onMouseLeave={() => setIsHovered(!isHovered)}
+      className="w-full h-fit flex flex-col gap-2 bg-white shadow-md hover:bg-[#146EB4] rounded-lg hover:text-white cursor-pointer"
+    >
+      <div className=" flex flex-col px-5 py-6 gap-4">
+        <div className="flex items-center gap-2 hover:text-white">
+          <p className="font-[400] text-sm xl:text-lg">{title}</p>
+          <span>
+            <Image
+              className="object-contain"
+              src={isHovered ? "/help2.png" : "/help.png"}
+              alt="help"
+              width={15}
+              height={15}
+            />
+          </span>
+        </div>
+
+        <div className="flex justify-between items-center">
+          <p className="text-sm lg:text-xl xl:text-3xl font-[500]">₹{amount}</p>
+          <span className="flex items-center gap-1">
+            <p className="text-xs lg:text-sm xl:text-base underline">
+              {orders}
+            </p>
+            {title !== "Amount Processed" && <MdArrowForwardIos />}
+          </span>
+        </div>
+      </div>
+
+      {isHovered && (
+        <div className="flex justify-between items-center px-5 py-2 rounded-lg text-xs lg:text-xs xl:text-sm bg-[#0E4F82] w-full">
+          <p>Next payout date:</p>
+          <p>Today, 04:00PM</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cards;
